24fe7acde8bc4291add9bac8f09b6a5e763f0535
Railson25 <railsonsantiago123@gmail.com>
2024-03-13 18:41:39 -0300

    feat(score): Champions score
    This commit will imporve our calcule with new parameters

--- components/horizontal-chart.tsx
+++ components/horizontal-chart.tsx
@@ -44,7 +44,7 @@ export const HorizontalChart = ({
 
     const getAverage = (property: string) => {
       return (
-        championInGame.reduce(
+        championGames.reduce(
           (accumulator, game: any) => accumulator + game[property],
           0
         ) / championGames.length
@@ -58,11 +58,47 @@ export const HorizontalChart = ({
     const avgGold = getAverage("gold");
     const avgDamage = getAverage("damage");
     const avgGoldAtFiveMin = getAverage("goldAtFiveMin") || 0;
+    const avgGoldAtTenMin = getAverage("goldAtTenMin") || 0;
+    const avgDamageByDeath = getAverage("damageByDeath") || 0;
+    const avgDamageSuffered = getAverage("damageSuffered") || 0;
+    const avgTeamParticipation = getAverage("teamParticipation") || 0;
+    const avgShieldOfCure = getAverage("shieldOfCure") || 0;
+    const avgEpicMonster = getAverage("epicMonster") || 0;
+    const avgWardNumber = getAverage("wardNumber") || 0;
+    const avgGoldDamageRate = getAverage("goldDamageRate") || 0;
+    const avgFarmMonster = getAverage("farmMonster") || 0;
+    const avgTroopScore = getAverage("troopScore") || 0;
+    const avgToppledTowers = getAverage("toppledTowers") || 0;
+    const avgWinRate = calculateAverage(
+      games.map((game) => (game.win ? 1 : 0))
+    );
+    const avgLoseRate = calculateAverage(
+      games.map((game) => (game.lose ? 1 : 0))
+    );
 
     const normalizedGameDuration = getNormalizedValue(avgGameDuration, 60);
     const normalizedGold = getNormalizedValue(avgGold, 100000);
     const normalizedDamage = getNormalizedValue(avgDamage, 200000);
     const normalizedGoldAtFiveMin = getNormalizedValue(avgGoldAtFiveMin, 15000);
+    const normalizedGoldAtTenMin = getNormalizedValue(avgGoldAtTenMin, 30000);
+    const normalizedDamageByDeath = getNormalizedValue(avgDamageByDeath, 10000);
+    const normalizedDamageSuffered = getNormalizedValue(
+      avgDamageSuffered,
+      20000
+    );
+    const normalizedTeamParticipation = getNormalizedValue(
+      avgTeamParticipation,
+      100
+    );
+    const normalizedShieldOfCure = getNormalizedValue(avgShieldOfCure, 5000);
+    const normalizedEpicMonster = getNormalizedValue(avgEpicMonster, 10);
+    const normalizedWardNumber = getNormalizedValue(avgWardNumber, 20);
+    const normalizedGoldDamageRate = getNormalizedValue(avgGoldDamageRate, 100);
+    const normalizedFarmMonster = getNormalizedValue(avgFarmMonster, 20);
+    const normalizedTroopScore = getNormalizedValue(avgTroopScore, 20);
+    const normalizedToppledTowers = getNormalizedValue(avgToppledTowers, 5);
+    const normalizedWinRate = getNormalizedValue(avgWinRate, 1);
+    const normalizedLoseRate = getNormalizedValue(avgLoseRate, 1);
 
     const avgTotalKills = calculateAverage(
       games.map((game) => game.totalKills)
@@ -81,13 +117,26 @@ export const HorizontalChart = ({
     const repeatedTerm = 0.05 * (1 / championGames.length);
 
     const score =
-      0.15 * normalizedGameDuration +
-      0.2 * normalizedGold +
-      0.25 * normalizedDamage +
-      0.05 * normalizedGoldAtFiveMin +
-      0.05 * normalizedGoldAtFiveMin +
+      0.05 * normalizedGameDuration +
+      0.05 * normalizedGold +
+      0.05 * normalizedDamage +
+      0.025 * normalizedGoldAtFiveMin +
+      0.025 * normalizedGoldAtTenMin +
+      0.025 * normalizedDamageByDeath +
+      0.025 * normalizedDamageSuffered +
+      0.025 * normalizedTeamParticipation +
+      0.025 * normalizedShieldOfCure +
+      0.025 * normalizedEpicMonster +
+      0.025 * normalizedWardNumber +
+      0.025 * normalizedGoldDamageRate +
+      0.025 * normalizedFarmMonster +
+      0.025 * normalizedTroopScore +
+      0.025 * normalizedToppledTowers +
+      0.05 * normalizedWinRate +
+      0.05 * normalizedLoseRate +
+      0.1 * normalizedTotalKills +
+      0.1 * normalizedTotalDeaths +
       15 * repeatedTerm;
-    0.1 * normalizedTotalKills + 0.1 * normalizedTotalDeaths;
 
     return score;
   };
