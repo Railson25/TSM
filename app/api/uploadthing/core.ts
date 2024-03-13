1a5449cf8209a2ea5260cae1e9a179de9c410c89
Railson25 <railsonsantiago123@gmail.com>
2024-03-13 14:26:16 -0300

    fix(uploadthing): authorization
    This commti will fiz correct authorization

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,28 +1,26 @@
 import { auth } from "@clerk/nextjs";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { isTeacher } from "@/lib/teacher";
- 
-const f = createUploadthing();
- 
-const handleAuth = () => {
-    const {userId} = auth()
-    const isAuthorized = isTeacher(userId)  
 
-    if(!userId || !isAuthorized) throw new Error('Unauthorized')
-    return {userId}
-}
- 
+const f = createUploadthing();
+
+const handleAuth = () => {
+  const { userId } = auth();
+
+  if (!userId) throw new Error("Unauthorized");
+  return { userId };
+};
+
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  courseImage: f({image: {maxFileSize: '4MB',  maxFileCount: 1}})
-    .middleware(() => handleAuth())
-    .onUploadComplete(() => {}) ,
- courseAttachment: f(['text', 'image', 'video', 'audio', 'pdf'])
+  courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
     .middleware(() => handleAuth())
     .onUploadComplete(() => {}),
- chapterVideo: f({video: {maxFileCount: 1, maxFileSize: "512GB"}})
- .middleware(() => handleAuth())
- .onUploadComplete(() => {})
+  courseAttachment: f(["text", "image", "video", "audio", "pdf"])
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+  chapterVideo: f({ video: { maxFileCount: 1, maxFileSize: "512GB" } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
 } satisfies FileRouter;
- 
+
 export type OurFileRouter = typeof ourFileRouter;
